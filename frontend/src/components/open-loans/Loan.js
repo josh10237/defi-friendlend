740a63906a6a7bbf64f131ec925f99d625be66a9
Joey Laderer <joeyladerer@me.com>
2023-11-09 13:47:12 -0800

    open loans static

---
+++ frontend/src/components/open-loans/Loan.js
@@ -0,0 +1,118 @@
+import { Text, CircularProgress, CircularProgressLabel, Flex, Center, Button, Box, Input, InputLeftAddon, InputGroup } from "@chakra-ui/react";
+import React from "react";
+
+function Loan({loan}) {
+
+    const portionFilled = loan.filled / loan.amount * 100
+    
+    // design constants
+    const textMargin = '15px'
+
+    return (
+        <Flex 
+            w={'25vw'} 
+            bg={'white'} 
+            borderColor={'lightgrey'}
+            borderWidth={'1px'}
+            borderRadius={'15px'}
+            direction={'column'}
+            margin={'5px'}
+            boxShadow={'base'}
+        >
+            <Center>
+                <Text>Loan: {loan.borrower}</Text>
+            </Center>
+            <Center>
+                <CircularProgress value={portionFilled} size={'100px'}>
+                    <CircularProgressLabel>
+                        {portionFilled}%
+                        </CircularProgressLabel>
+                </CircularProgress>
+            </Center>
+
+            <Text marginLeft={textMargin} textColor={'gray'}>User Info</Text>
+
+            <Flex justify={'space-between'} 
+                marginLeft={textMargin}
+                marginRight={textMargin}
+            >
+                <Text>Username</Text>
+                <Text>Josh</Text>
+            </Flex>
+
+            <Flex justify={'space-between'} 
+                marginLeft={textMargin}
+                marginRight={textMargin}
+            >
+                <Text>FriendLend Score</Text>
+                <Text>760</Text>
+            </Flex>
+
+            <Text 
+            marginLeft={textMargin} 
+            marginTop={textMargin}
+            textColor={'gray'}>Loan Stats</Text>
+
+            <Flex justify={'space-between'} 
+                marginLeft={textMargin}
+                marginRight={textMargin}
+            >
+                <Text>Amount</Text>
+                <Text>${loan.amount}</Text>
+            </Flex>
+
+            <Flex justify={'space-between'} 
+                marginLeft={textMargin}
+                marginRight={textMargin}
+            >
+                <Text>Due Date</Text>
+                <Text>{loan.dueDate}</Text>
+            </Flex>
+
+            <Flex justify={'space-between'} 
+                marginLeft={textMargin}
+                marginRight={textMargin}
+            >
+                <Text>Interest</Text>
+                <Text>10%</Text>
+            </Flex>
+
+            <Flex justify={'space-between'} 
+                marginLeft={textMargin}
+                marginRight={textMargin}
+            >
+                <Text>Daily Rate</Text>
+                <Text>2.5%</Text>
+            </Flex>
+
+            <Center
+                bg={'gray'}
+                h={'3px'}
+                marginLeft={'15px'}
+                marginRight={'15px'}
+                marginTop={'5px'}
+                marginBottom={'5px'}
+            />
+
+            <Center>
+                <InputGroup
+                    margin={'5px'}
+                    w={'15vw'}
+                >
+                    <InputLeftAddon>$</InputLeftAddon>
+                    <Input placeholder="Enter Amount"></Input>
+                </InputGroup>
+                <Button margin={'5px'} bg={'lightgreen'}>
+                    Contribute
+                </Button>
+            </Center>
+            
+            
+            
+
+
+        </Flex>
+    )
+}
+
+export default Loan
--- frontend/src/components/open-loans/OpenLoans.js
+++ frontend/src/components/open-loans/OpenLoans.js
@@ -1,11 +1,60 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import React from "react";
+import Loan from "./Loan";
+
+// PROPS NEEDED
+// list of open loans
+// 
 
 function OpenLoans() {
+    const openLoans = [
+        {
+            borrower: "LOAN 1",
+            amount: 2000,
+            filled: 1500,
+            dueDate: "December 1",
+            isFulfilled: false,
+        },
+        {
+            borrower: "LOAN 2",
+            amount: 2500,
+            filled: 1200,
+            dueDate: "December 2",
+            isFulfilled: false,
+        },
+        {
+            borrower: "LOAN 3",
+            amount: 750,
+            filled: 150,
+            dueDate: "December 3",
+            isFulfilled: false,
+        }
+    ]
     return (
-        <Flex>
-            OPEN LOANS
-        </Flex>
+        <Box
+            marginLeft={'10vw'}
+            marginRight={'10vw'}
+            borderRadius={'30px'}
+            borderWidth={'1px'}
+            boxShadow={'base'}
+        >
+            <Box 
+                marginLeft={'25px'} 
+                fontSize={'x-large'}
+                as={"b"}
+                textColor={'darkgray'}
+                >Open Loans</Box>
+            <Wrap justify={'center'}>
+                {openLoans.map((loan) => {
+                    return (
+                        <WrapItem>
+                            <Loan loan={loan} />
+                        </WrapItem>
+                    )
+                })
+                }
+            </Wrap>
+        </Box>
     )
 }
 
