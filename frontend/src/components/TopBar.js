7422c1c654d9cf958acd45e38d83c7862f87b0c2
Josh Benson <josh.benson.805@gmail.com>
2023-12-04 16:12:12 -0800

    updated top bar

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { Box, Center } from "@chakra-ui/react";
+import logo from './logo1.png';
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 function TopBar() {
     return (
-        <Center>
-            <Box bg={"lightgray"} w={'100%'} h={'7vh'} textAlign={'center'}>
+        <Flex 
+            w="100%" 
+            h="7vh" 
+            bgGradient="linear(to top right, purple, blue)"
+            align="center"
+            justify="space-between"
+        >
+            <Image src={logo} alt="Logo FL" boxSize="10vh" ml="2vh" />
+            <Box 
+                flex="1" 
+                textAlign="center" 
+                color="white" 
+                fontWeight="bold"
+            >
                 FRIENDLEND
             </Box>
-        </Center>
+            <Box boxSize="5vh" mr="2vh"/> {/* Placeholder for right alignment */}
+        </Flex>
     )
 }
 
-export default TopBar
+export default TopBar;
